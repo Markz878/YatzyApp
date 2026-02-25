69b84010fff4ca1f60a2556ce9e38eb963764cb4
Markz878 <markus_nisula@hotmail.com>
2026-02-25 22:53:04 +0200

    Code style changes to .NET 10

--- YatzyApp/Program.cs
+++ YatzyApp/Program.cs
@@ -1,19 +1,12 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using YatzyApp;
 using YatzyApp.Extensions;
 
-namespace YatzyApp;
-
-public class Program
-{
-    public static async Task Main(string[] args)
-    {
-        WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.Services.AddBlazoredLocalStorage();
-        builder.Services.AddLocalization();
-        WebAssemblyHost host = builder.Build();
-        await host.SetDefaultCulture();
-        await host.RunAsync();
-    }
-}
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddLocalization();
+WebAssemblyHost host = builder.Build();
+await host.SetDefaultCulture();
+await host.RunAsync();
